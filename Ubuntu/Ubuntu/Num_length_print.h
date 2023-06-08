a0d57a7b13290c8d2af6c8dcb4b7668bd4142b9c
liuhonghi <23306244@qq.com>
2023-06-08 10:23:03 +0800

    First Commit

---
+++ Ubuntu/Ubuntu/Num_length_print.h
@@ -0,0 +1,12 @@
+//
+//  Num_length_print.h
+//  Ubuntu
+//
+//  Created by HiLau on 2023/6/8.
+//
+
+#ifndef Num_length_print_h
+#define Num_length_print_h
+
+void* Num_length_print(void* args);
+#endif /* Num_length_print_h */
